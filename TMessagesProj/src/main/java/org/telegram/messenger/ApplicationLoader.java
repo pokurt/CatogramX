6b5d85b38efb1d752da2b2a4c877f68bae6c3459
thermatk <thermatk@thermatk.com>
2019-01-26 14:49:13 +0100

    [TF][PUSH] push service adjustments

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -30,6 +30,7 @@ import android.os.PowerManager;
 import android.os.SystemClock;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -46,6 +47,7 @@ import org.telegram.ui.Components.ForegroundDetector;
 import java.io.File;
 
 public class ApplicationLoader extends Application {
+    private static PendingIntent pendingIntent;
 
     @SuppressLint("StaticFieldLeak")
     public static volatile Context applicationContext;
@@ -235,10 +237,19 @@ public class ApplicationLoader extends Application {
             enabled = MessagesController.getMainSettings(UserConfig.selectedAccount).getBoolean("keepAliveService", !ApplicationLoader.hasPlayServices);
         }
         if (enabled) {
-            try {
-                applicationContext.startService(new Intent(applicationContext, NotificationsService.class));
-            } catch (Throwable ignore) {
+            Log.d("TFOSS", "Trying to start push service every minute");
+            // Telegram-FOSS: unconditionally enable push service
+            AlarmManager am = (AlarmManager) applicationContext.getSystemService(Context.ALARM_SERVICE);
+            Intent i = new Intent(applicationContext, NotificationsService.class);
+            pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, i, 0);
 
+            am.cancel(pendingIntent);
+            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 60000, pendingIntent);
+            try {
+                Log.d("TFOSS", "Starting push service...");
+                applicationContext.startService(new Intent(applicationContext, NotificationsService.class));
+            } catch (Throwable e) {
+                Log.d("TFOSS", "Failed to start push service");
             }
         } else {
             applicationContext.stopService(new Intent(applicationContext, NotificationsService.class));
@@ -247,6 +258,9 @@ public class ApplicationLoader extends Application {
             pintent = PendingIntent.getService(applicationContext, 0, new Intent(applicationContext, NotificationsService.class), PendingIntent.FLAG_MUTABLE);
             AlarmManager alarm = (AlarmManager)applicationContext.getSystemService(Context.ALARM_SERVICE);
             alarm.cancel(pintent);
+	        if (pendingIntent != null) {
+	            alarm.cancel(pendingIntent);
+	        }
         }
     }
 
