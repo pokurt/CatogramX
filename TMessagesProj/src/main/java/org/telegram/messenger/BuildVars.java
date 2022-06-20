firexgram/firexgram

Do not send to official update page
based on https://github.com/Telegram-FOSS-Team/Telegram-FOSS/commit/5404252a0919fdf47303bbd9485d5de2763eac2e and https://github.com/Telegram-FOSS-Team/Telegram-FOSS/commit/f68d97e9a2bcd702ab6f650f0c3418a9b650eda6

Change-Id: Ib4312337d9604f1a39c05403dba1e175aa0af13b

TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -26,7 +26,7 @@ public class BuildVars {
     public static String APP_HASH = BuildConfig.APP_HASH; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
 
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
+    public static String PLAYSTORE_APP_URL = "https://github.com/nift4/Telegram/releases/latest";
 
     static {
         if (ApplicationLoader.applicationContext != null) {

pokurt/CatogramX

TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -29,7 +29,7 @@ public class BuildVars {
     public static String APP_HASH = BuildConfig.APP_HASH; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
 
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=it.owlgram.android";
+    public static String PLAYSTORE_APP_URL = "https://github.com/CatogramX/CatogramX/releases/latest";
 
     static {
         if (ApplicationLoader.applicationContext != null) {